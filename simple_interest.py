96791868b46c07a1b79b8e3743887a1e6f509ced
Chimennadi <nnadichime04@gmail.com>
2024-05-21 00:51:06 +0100

    python practice

---
+++ simple_interest.py
@@ -0,0 +1,12 @@
+# p = int(input("Please enter p "))
+# n = int(input("Please enter n "))
+# r = int(input("Please enter r "))
+
+# si = (p*n*r)/100
+
+# print(si)
+counter = 0
+
+while (counter < 10):
+    print(counter)
+    counter += 1
